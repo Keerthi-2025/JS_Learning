348cf2860f64df79cb3bac0cb82a5e30bee432df
Keerthi <rhkeerthi3@gmail.com>
2025-06-27 20:57:23 +0530

    Initial commit

---
+++ js-1.jsx
@@ -0,0 +1,22 @@
+ /* Comments in JS are of 2 types
+
+1. Inline commnets 
+
+var a = 5; 
+
+2. Multile commnets */
+
+//DataTypes --> undefined = something that has not been defined, 
+// null = means nothing,
+//boolen = it has value either true or false
+// symbol = unique value
+//number, objects
+
+
+
+
+
+
+
+
+
