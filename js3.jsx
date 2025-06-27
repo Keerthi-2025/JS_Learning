c21cf44098cfbf58ef20ad90c7cbdef9d50b0c91
Keerthi <rhkeerthi3@gmail.com>
2025-06-28 00:46:24 +0530

    Methods used in Arrays

---
+++ js3.jsx
@@ -0,0 +1,27 @@
+//Accessing array elements
+var ourArray = [10,20,30];
+ourArr[1] = ourArray[22];
+console.log(ourArr)
+
+
+//push method in array
+var ourArray = ["jon", "cat"];
+ourArray.push(["happ job got both"]);
+
+
+//pop method in array  (rremove last elememt i.e 3)
+var ourArray = [1,2,3];
+var result = ourArray.pop();
+console.log(result)
+ 
+//shift method in array   (removes the first element in the array)
+var arr = ["jon","car",["dog"]];
+var res = arr.shift();
+
+//unshift method in array (adds the element to the first place)
+var arr = ["jon","cat","dog"];
+arr.shift();
+arr.unshift("cars");
+
+//shopping list
+var mylist = [["apple",2],["banana",2]];
