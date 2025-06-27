e9f47a1fef1f262c939eecf4b9b9aeb57796bece
Keerthi <rhkeerthi3@gmail.com>
2025-06-27 21:38:33 +0530

    Variables concept

---
+++ js2.jsx
@@ -0,0 +1,64 @@
+//3 ways of Varibale Declaration
+
+let name = "javalearning"
+
+const pi = 3.14
+
+var name2 = "javalearning"
+
+
+//Storing and assignment operator
+var a;
+var a= 10;
+console.log(a)
+
+//Initialization of variables
+var ab = 9;
+
+
+var a = 5;
+var b = 10;
+var c = "I am string";
+
+a = a + 1;         // 6
+b = b +1;           // 11
+c = c + "Component";  // T am string component
+
+//Basic Math
+var sum = 10 +10;       //Addition
+console.log(sum)
+
+var diff = 40 - 10;     //Subtraction
+console.log(diff)
+
+var mul = 10 *10;       //Multiplication
+console.log(mul)
+
+var quot = 10 /5;      // Quotient
+console.log(quot)
+
+var myvar = 89;
+myvar = myvar +1;
+myvar++;
+
+var myvar = 89;
+myvar = myvar -1;
+myvar--;
+
+var deci = 5.6;
+
+var deci = 5.6 * 5.6;       // Multiplying Decimal
+console.log(deci)
+
+var deci = 5.6 / 5.6;       //Dividing Decimal
+console.log(deci)
+
+var rem;
+rem = 10%5;
+console.log(rem)
+
+a = a + 3;          //Augmented operators
+a +=3;
+
+b = b - 2;
+b-=2;
