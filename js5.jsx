96ff3c716b2527dd1fec2b900c3756ea6ad8e1ff
Keerthi <rhkeerthi3@gmail.com>
2025-06-28 19:32:17 +0530

    Return value from function

---
+++ js5.jsx
@@ -0,0 +1,25 @@
+//Return value from function
+function fun(num){
+    return num -  2;
+}
+fun(10);
+// or console.log(fun(10));
+
+function mul(num){
+    return mul *10;
+}
+console.log(mul(10));
+
+//Assisgnment with return value
+var changed = 0;
+function fun1(num){
+    return num + 2;
+}
+
+changed = fun1(10);
+
+var processed = 0;
+function demo(number){
+    return number - 10;
+}
+processed = demo(10);
